355e56ebe1457279a24d3f66b2d4994889415c57
Aaditya <22BCT10047@cuchd.in>
2026-05-16 00:35:21 +0530

    LC(58)

--- NeetCode/58.length-of-last-word.py
+++ NeetCode/58.length-of-last-word.py
@@ -7,6 +7,13 @@
 # @lc code=start
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        
+        cnt = 0
+        n = len(s)
+        for n in range(n-1,-1,-1):
+            if s[n]!=" ":
+                cnt+=1
+            elif cnt>0:
+                break
+        return cnt 
 # @lc code=end
 
